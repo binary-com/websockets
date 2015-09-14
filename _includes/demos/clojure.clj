a84096ae69e4816c903dda33ff7c69ab34653572
Jean-Yves Sireau <jy@binary.com>
2015-09-14 13:54:53 +0800

    clojure

---
+++ _includes/demos/clojure.clj
@@ -0,0 +1,13 @@
+(ns binaryws.core
+  (:require [gniazdo.core :as ws]) ;; [stylefruits/gniazdo "0.4.0"]
+  (:gen-class))
+(defn -main
+  [& args]
+  (def socket
+   (ws/connect
+    "wss://www.binary.com/websockets/v2"
+    :on-receive #(prn 'received %)))
+  (ws/send-msg socket "{ \"ticks\": \"R_50\"}")
+  (Thread/sleep 5000)
+  (ws/close socket)
+)
