1806ab03d96447f050bcec30068c279dca321356
Jean-Yves Sireau <jy@binary.com>
2015-09-14 16:31:44 +0800

    Update clojure.clj

--- _includes/demos/clojure.clj
+++ _includes/demos/clojure.clj
@@ -1,13 +1,14 @@
 (ns binaryws.core
-  (:require [gniazdo.core :as ws]) ;; [stylefruits/gniazdo "0.4.0"]
+   (:require [gniazdo.core :as ws]) ;; [stylefruits/gniazdo "0.4.0"]
+   (:require [cheshire.core :refer :all]) ;; [cheshire "5.5.0"]
   (:gen-class))
 (defn -main
   [& args]
   (def socket
    (ws/connect
     "wss://www.binary.com/websockets/v2"
-    :on-receive #(prn 'received %)))
-  (ws/send-msg socket "{ \"ticks\": \"R_100\"}")
+    :on-receive #(prn (parse-string %))))
+  (ws/send-msg socket (generate-string {:ticks "R_100"}))
   (Thread/sleep 5000)
   (ws/close socket)
 )
