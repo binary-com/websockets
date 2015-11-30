5df26524ac2a1097ad06ddd49ccdec5e00df1f55
Jean-Yves Sireau <jy@binary.com>
2015-11-30 16:09:17 +0800

    Update elixir.exs

--- _includes/demos/elixir.exs
+++ _includes/demos/elixir.exs
@@ -1,7 +1,7 @@
 # {:socket, "~> 0.3.0"}
 # {:poison, "~> 1.5"}
 {:ok, omsg}  = Poison.encode(%{:ticks => "R_100"})
-socket = Socket.Web.connect! "www.binary.com", 443, [{:secure, true}, {:path, "/websockets/v2"}]
+socket = Socket.Web.connect! "ws.binaryws.com", 443, [{:secure, true}, {:path, "/websockets/v3"}]
 socket |> Socket.Web.send! {:text, omsg}
 {:text, imsg} = socket |> Socket.Web.recv!
 IO.puts "ticks update: " <> imsg
