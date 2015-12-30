e903d5a1694734da00f88c8b024277cd315a3ca8
Boris Yankov <borisyankov@gmail.com>
2015-12-30 16:53:59 +0200

    Update C# demo with TLS enabled

--- _includes/demos/csharp.cs
+++ _includes/demos/csharp.cs
@@ -1,30 +1,41 @@
-class Program
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Net;
+
+namespace ConsoleApp
 {
-    static async Task SendTicksRequest()
+    class Program
     {
-        var ws = new ClientWebSocket();
-        var uri = new Uri("wss://ws.binaryws.com/websockets/v3");
+        static async Task SendTicksRequest()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+            var ws = new ClientWebSocket();
+            var uri = new Uri("wss://ws.binaryws.com/websockets/v3");
 
-        await ws.ConnectAsync(uri, CancellationToken.None);
+            await ws.ConnectAsync(uri, CancellationToken.None);
 
-        var reqAsBytes = Encoding.UTF8.GetBytes("{\"ticks\":\"R_100\"}");
-        var ticksRequest = new ArraySegment&lt;byte>(reqAsBytes);
+            var reqAsBytes = Encoding.UTF8.GetBytes("{\"ticks\":\"R_100\"}");
+            var ticksRequest = new ArraySegment<byte>(reqAsBytes);
 
-        await ws.SendAsync(ticksRequest,
-            WebSocketMessageType.Text,
-            true,
-            CancellationToken.None);
+            await ws.SendAsync(ticksRequest,
+                WebSocketMessageType.Text,
+                true,
+                CancellationToken.None);
 
-        var buffer = new ArraySegment&lt;byte>(new byte[1024]);
-        var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+            var buffer = new ArraySegment<byte>(new byte[1024]);
+            var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
 
-        string response = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
-        Console.WriteLine(response);
-    }
+            string response = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+            Console.WriteLine(response);
+        }
 
-    static void Main(string[] args)
-    {
-        SendTicksRequest();
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            SendTicksRequest();
+            Console.ReadLine();
+        }
     }
 }
