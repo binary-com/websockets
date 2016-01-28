bc089405961bae9ab443ee0f39dcf123d328e0a7
Boris Yankov <borisyankov@gmail.com>
2016-01-28 05:46:26 +0200

    Fix C# demo html issue

--- _includes/demos/csharp.cs
+++ _includes/demos/csharp.cs
@@ -18,14 +18,14 @@ namespace ConsoleApp
             await ws.ConnectAsync(uri, CancellationToken.None);
 
             var reqAsBytes = Encoding.UTF8.GetBytes("{\"ticks\":\"R_100\"}");
-            var ticksRequest = new ArraySegment<byte>(reqAsBytes);
+            var ticksRequest = new ArraySegment&lt;byte>(reqAsBytes);
 
             await ws.SendAsync(ticksRequest,
                 WebSocketMessageType.Text,
                 true,
                 CancellationToken.None);
 
-            var buffer = new ArraySegment<byte>(new byte[1024]);
+            var buffer = new ArraySegment&lt;byte>(new byte[1024]);
             var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
 
             string response = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
