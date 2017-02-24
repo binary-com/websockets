dcc7b1c12708f0b70bdfea9eb37aed83ce4835ce
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2017-02-24 12:42:39 +0800

    Update go.go

--- _includes/demos/go.go
+++ _includes/demos/go.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
 import "golang.org/x/net/websocket"
 
-var origin = "http://localhost/"
-var url = "wss://ws.binaryws.com/websockets/v3?app_id=1089"
-
 func main() {
-    ws, _ := websocket.Dial(url, "", origin)
+	ws, _ := websocket.Dial("wss://ws.binaryws.com/websockets/v3?app_id=1089", "", "http://localhost/")
 
-    message := []byte("{\"ticks\": \"R_100\"}")
-    ws.Write(message)
-    fmt.Printf("Send: %s\n", message)
+	ws.Write([]byte(`{"ticks": "R_100"}`))
 
-    var msg = make([]byte, 512000)
-    for {
-        ws.Read(msg)
-        fmt.Printf("Receive: %s\n", msg)
-    }
+	var msg = make([]byte, 512000)
+	for {
+		ws.Read(msg)
+		println("Received: ", string(msg))
+	}
 }
