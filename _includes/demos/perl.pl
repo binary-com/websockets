e165584306b114bc04ad9dbdc50622786af6ad04
Tom Molesworth <tom@binary.com>
2018-08-19 15:13:01 +0800

    Fix error handling (#127)
    This should not rely on STDOUT happening to accept our data.

--- _includes/demos/perl.pl
+++ _includes/demos/perl.pl
@@ -7,7 +7,10 @@ use Data::Dumper;
 my $ua = Mojo::UserAgent->new;
 $ua->websocket('wss://ws.binaryws.com/websockets/v3?app_id=1089' => sub {
     my ($ua, $tx) = @_;
-    say 'WebSocket handshake failed!' and return unless $tx->is_websocket;
+    unless($tx->is_websocket) {
+        say 'WebSocket handshake failed!';
+        return;
+    }
 
     $tx->on(json => sub {
         my ($tx, $data) = @_;
