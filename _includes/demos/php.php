0d90c3ae30e0338e4200f2ee2cb7cd0ace7ae341
Albert Jonathan <albert.jonathan23@gmail.com>
2019-07-20 01:58:56 +0700

    Update PHP Sample Code
    devristo/phpws isn't maintained anymore and caused an error on PHP 7 + Laravel when installed via composer.
    
    Better use textalk/websocket. Simpler to use and support PHP 7.

--- _includes/demos/php.php
+++ _includes/demos/php.php
@@ -1,21 +1,7 @@
-require_once("vendor/autoload.php");
+use WebSocket\Client;
+use Illuminate\Support\Facades\Log;
 
-$loop = \React\EventLoop\Factory::create();
-
-$logger = new \Zend\Log\Logger();
-$writer = new Zend\Log\Writer\Stream("php://output");
-$logger->addWriter($writer);
-
-$client = new \Devristo\Phpws\Client\WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089", $loop, $logger);
-
-$client->on("connect", function($headers) use ($client, $logger){
-    $logger->notice("connected!");
-    $client->send("{\"ticks\":\"R_100\"}");
-});
-
-$client->on("message", function($message) use ($client, $logger){
-    $logger->notice("ticks update: ".$message->getData());
-});
-
-$client->open();
-$loop->run();
+$client = new Client("wss://ws.binaryws.com/websockets/v3?app_id=1089");
+$request = (object) ['ticks' => 'R_100'] ;
+$client->send(json_encode($request));
+Log::info("ticks update: ". $client->receive());
