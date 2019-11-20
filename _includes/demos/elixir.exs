ea99d21946f34732765c282e067f004bef640760
Zak B. Elep <zakame@zakame.net>
2019-11-20 15:05:53 +0800

    More updates of code samples
    Update demos for Golang, Elixir, and Scala (based largely on
    https://github.com/zakame/binary-websockets-demos)

--- _includes/demos/elixir.exs
+++ _includes/demos/elixir.exs
@@ -1,7 +1,23 @@
-# {:socket, "~> 0.3.0"}
-# {:poison, "~> 1.5"}
-{:ok, omsg}  = Poison.encode(%{:ticks => "R_100"})
-socket = Socket.Web.connect! "ws.binaryws.com", 443, [{:secure, true}, {:path, "/websockets/v3?app_id=1089"}]
-socket |> Socket.Web.send! {:text, omsg}
-{:text, imsg} = socket |> Socket.Web.recv!
-IO.puts "ticks update: " <> imsg
+# Build as an escript, cf https://elixirschool.com/en/lessons/advanced/escripts/
+# Depends on: [{:socket, "~> 0.3"}, {:poison, "~> 4.0"}]
+defmodule BinaryWS do
+  def main(_args) do
+    {:ok, omsg} = Poison.encode(%{"ticks" => "R_100"})
+    socket = Socket.Web.connect! "ws.binaryws.com", 443,
+      [{:secure, true}, {:path, "/websockets/v3?app_id=1089"}]
+    socket |> Socket.Web.send!({:text, omsg})
+    spawn_link(fn -> loop(socket) end)
+    :timer.sleep(10000)
+    socket |> Socket.Web.close
+  end
+
+  # Start receiving updates in another process until main process wakes up.
+  defp loop(socket) do
+    case socket |> Socket.Web.recv! do
+      {:text, imsg} ->
+        IO.puts "ticks update: " <> imsg
+        loop(socket)
+      _ -> exit(:normal)
+    end
+  end
+end
--- _includes/demos/go.go
+++ _includes/demos/go.go
@@ -1,15 +1,57 @@
 package main
 
-import "golang.org/x/net/websocket"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+func receive(ticks chan string, ws *websocket.Conn, done <-chan struct{}) {
+	defer close(ticks)
+	var msg string
+	for {
+		if err := websocket.Message.Receive(ws, &msg); err != nil {
+			log.Fatalln("recv:", err)
+		}
+		select {
+		case <-done:
+			return
+		case ticks <- msg:
+		}
+	}
+}
 
 func main() {
-	ws, _ := websocket.Dial("wss://ws.binaryws.com/websockets/v3?app_id=1089", "", "http://localhost/")
+	origin := "http://localhost"
+	url := "wss://ws.binaryws.com/websockets/v3?app_id=1089"
 
-	ws.Write([]byte(`{"ticks": "R_100"}`))
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Fatalln("dial:", err)
+	}
+	defer ws.Close()
 
-	var msg = make([]byte, 512000)
+	var (
+		done  = make(chan struct{})
+		ticks = make(chan string)
+	)
+	go receive(ticks, ws, done)
+
+	if err = websocket.Message.Send(ws, `{"ticks": "R_100"}`); err != nil {
+		log.Fatalln("send:", err)
+	}
+
+	timeout := time.After(10 * time.Second)
+
 	for {
-		ws.Read(msg)
-		println("Received: ", string(msg))
+		select {
+		case tick := <-ticks:
+			fmt.Println("ticks update:", tick)
+		case <-timeout:
+			done <- struct{}{}
+			return
+		}
 	}
 }
